032948a106853ce0ce3cc0b7fc4fc8149e975ee5
rvs101 <raulvelasalas@gmail.com>
2022-03-24 13:14:41 +0100

    1º commit - ejemplos

---
+++ T2/src/ejemplos/metodos/EjemploMetodo.java
@@ -0,0 +1,19 @@
+package ejemplos.metodos;
+
+/**
+ * 
+ * @author Robot
+ *
+ */
+public class EjemploMetodo {
+
+	/**
+	 * 
+	 * @param a
+	 * @param b
+	 * @return
+	 */
+	public static int suma(int a, int b) {
+		return (a + b);
+	}
+}
