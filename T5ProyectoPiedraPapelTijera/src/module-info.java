d61dacdf08f3c3a618592990aa64cc776b2bbd68
rvs101 <raulvelasalas@gmail.com>
2022-04-04 23:51:21 +0200

    9º Mi maravilloso juego de consola Piedra Papel y Tijera

---
+++ T5ProyectoPiedraPapelTijera/src/module-info.java
@@ -0,0 +1,40 @@
+/**
+ * • PARTES DEL PROGRAMA
+ * 
+ * → Explicarle al jugador como se juega
+ * 
+ * → Generar la jugada alteatoria del ordenador
+ * 
+ * → Pedir al jugador su jugada mediante una letra :
+ * 
+ * → P para piedra L para papel , T para Tijeras , S para salir terminando la
+ * partida)
+ *
+ * → Decidir quién ha ganado
+ * 
+ * *************************************
+ *
+ * ◘ CONSTRUIR EL PROGRAMA
+ * 
+ * → Preparar el esqueleto y las constantes necesarias
+ * 
+ * → Mostrar instrucciones al usuario
+ * 
+ * → Generar la jugada del ordenador
+ * 
+ * → Recoger la jugada del usuario
+ * 
+ * → Interpretar la jugada del usuario
+ * 
+ * → Calcular el ganador de la jugada
+ * 
+ * → Mostrar el resultado de la jugada
+ * 
+ * → Hacer el juego repetitivo
+ * 
+ * @author Robot
+ *
+ */
+module T5ProyectoPiedraPapelTijera {
+
+}
---
+++ T5ProyectoPiedraPapelTijera/src/programa/T5ClasePrincipal.java
@@ -0,0 +1,15 @@
+package programa;
+
+/**
+ * Clase Principal del programa
+ * 
+ * @author RVS
+ *
+ */
+public class T5ClasePrincipal {
+
+	public static void main(String[] args) {
+		T5LogicaJuego.comenzarJuego();
+		
+	}
+}
